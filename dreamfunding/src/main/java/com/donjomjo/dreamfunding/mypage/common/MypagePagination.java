1105934eb52586a5871e699399577e74d9fe6f39
smallyong1 <72965078+smallyong1@users.noreply.github.com>
2021-01-04 01:17:47 +0900

    fix mypage

--- dreamfunding/src/main/java/com/donjomjo/dreamfunding/mypage/common/MypagePagination.java
+++ dreamfunding/src/main/java/com/donjomjo/dreamfunding/mypage/common/MypagePagination.java
@@ -4,19 +4,15 @@ import com.donjomjo.dreamfunding.mypage.common.model.vo.MypagePageInfo;
 
 public class MypagePagination {
 	
-	public static MypagePageInfo getMypagePageInfo(int listCount, int currentPage, int pageLimit, int fundingLimit) {
-		// * maxPage : listCount, boardLimit 영향을 받는다.
-		int maxPage = (int)Math.ceil((double)listCount/fundingLimit);
-		// * startPage : currentPage, pageLimit 영향을 받는다 
+	public static MypagePageInfo getMypagePageInfo(int listCount, int currentPage, int pageLimit, int mypageLimit) {
+		
+		int maxPage = (int)Math.ceil((double)listCount/mypageLimit);
 		int startPage = (currentPage - 1) / pageLimit * pageLimit + 1;
-		// * endPage : startPage, pageLimit, maxPage
 		int endPage = startPage + pageLimit - 1;
 		if(endPage > maxPage) {
 			endPage = maxPage;
 		}
-		
-		return new MypagePageInfo(listCount, currentPage, startPage, endPage, maxPage, pageLimit, fundingLimit);
-		
+		return new MypagePageInfo(listCount, currentPage, startPage, endPage, maxPage, pageLimit, mypageLimit);
 	}
-
+	
 }
--- dreamfunding/src/main/java/com/donjomjo/dreamfunding/mypage/model/dao/MypageDao.java
+++ dreamfunding/src/main/java/com/donjomjo/dreamfunding/mypage/model/dao/MypageDao.java
@@ -1,29 +1,46 @@
 package com.donjomjo.dreamfunding.mypage.model.dao;
 
-
 import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.ibatis.session.RowBounds;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.donjomjo.dreamfunding.mypage.common.model.vo.MypagePageInfo;
-import com.donjomjo.dreamfunding.mypage.model.vo.Funding;
+import com.donjomjo.dreamfunding.mypage.model.vo.FundingProject;
+import com.donjomjo.dreamfunding.mypage.model.vo.LikeProject;
 
 @Repository
 public class MypageDao {
-	
-	//funding 
-	
+
+
+	//펀딩한 프로젝트 리스트 갯수
 	public int selectFundingListCount(SqlSessionTemplate sqlSession) {
 		return sqlSession.selectOne("mypageMapper.selectFundingListCount");
 	}
+	// 펀딩한 프로젝트 리스트 조회
+	public List<Object> selectFundingList(SqlSessionTemplate sqlSession, MypagePageInfo mypi){
+		int offset = (mypi.getCurrentPage()-1) * mypi.getMypageLimit();
+		RowBounds rowBounds = new RowBounds (offset, mypi.getMypageLimit());
 	
-	public ArrayList<Funding> selectFundingList(SqlSessionTemplate sqlSession, MypagePageInfo mypi){
-		int offset=(mypi.getCurrentPage() -1)* mypi.getFundingLimit();
-		RowBounds rowBounds = new RowBounds(offset, mypi.getFundingLimit());
-		
-		return (ArrayList)sqlSession.selectList("mypageMapper.selectFundingList", null,rowBounds);
+		return (ArrayList)sqlSession.selectList("mypageMapper.selectFundingList", null, rowBounds);
 	}
-
+	
+	
+		
+	
+	//관심있는 프로젝트 리스트 갯수
+	public int selectLikeListCount(SqlSessionTemplate sqlSession) {
+		return sqlSession.selectOne("mypageMapper.selectLikeListCount");
+	}
+	
+	//관심있는 프로젝트 리스트 조회
+	public List<Object> selectLikeList(SqlSessionTemplate sqlSession, MypagePageInfo mypi){
+		int offset = (mypi.getCurrentPage()-1) * mypi.getMypageLimit();
+		RowBounds rowBounds = new RowBounds (offset, mypi.getMypageLimit());
+		
+		return (ArrayList)sqlSession.selectList("mypageMapper.selectLikeList", null, rowBounds);
+	}
+	
 }
